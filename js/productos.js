7f983c7cbd8d7b45ddc1ac7def749c98006b3944
FacuDurdos <facudurdos@gmail.com>
2024-07-30 20:33:35 -0300

    tercer commit

--- js/productos.js
+++ js/productos.js
@@ -14,29 +14,37 @@ let stockBebidas = [5, 5, 5, 5, 5];
 let stockFrescos = [5, 5, 5, 5, 5];
 let stockLimpieza = [5, 5, 5, 5, 5];
 let stockHogar = [5, 5, 5, 5, 5];
-
+let total = 0
 
 let llenarProductos = function (categoria, productos, precios, stock) {
    let padre = document.getElementById(categoria)
    for (let i = 0; i < productos.length; i++) {
       let card = padre.children[i];
-      card.getElementsByTagName("h1")[0].innerHTML=productos[i]
-      card.getElementsByTagName("h2")[0].innerHTML=`$ ${precios[i]}`
-      card.getElementsByTagName("h4")[0].innerHTML=`Productos disponibles: ${stock[i]}`
-      card.getElementsByTagName("button")[0].addEventListener("click", () => {
-         let cantidad = card.getElementsByTagName("input")[0].value
-         if (cantidad > 0 && stock[i] >= cantidad) {
-            alert(`Productos comprados: Total $${cantidad*precios[i]}`)
-            stock[i] -= cantidad
-            card.getElementsByTagName("h4")[0].innerHTML=stock[i]
-         } else {
-            alert("ingrese una cantidad mayor a cero o no hay stock")
-         }
-      })
+      card.getElementsByTagName("h1")[0].innerHTML = productos[i]
+      card.getElementsByTagName("h2")[0].innerHTML = `$ ${precios[i]}`
+      card.getElementsByTagName("h4")[0].innerHTML = `Productos disponibles: ${stock[i]}`
+      card.getElementsByTagName("button")[0].addEventListener("click",()=>comprar(card, i, precios, stock))
    }
 }
-llenarProductos("almacen",almacen,preciosAlmacen,stockAlmacen);
-llenarProductos("bebidas",bebidas, preciosBebidas,stockBebidas);
-llenarProductos("frescos",frescos,preciosFrescos,stockFrescos);
-llenarProductos("limpieza",limpieza,preciosLimpieza,stockLimpieza);
-llenarProductos("hogar",hogar,preciosHogar,stockHogar);
+
+let comprar = function (card, i, precios, stock) {
+   let cantidad = card.getElementsByTagName("input")[0].value
+   if (cantidad > 0 && stock[i] >= cantidad) {
+      total += cantidad * precios[i]
+      stock[i] -= cantidad
+      card.getElementsByTagName("h4")[0].innerHTML = stock[i]
+   } else {
+      alert("ingrese una cantidad mayor a cero o no hay stock disponible")
+   }
+}
+
+
+
+llenarProductos("almacen", almacen, preciosAlmacen, stockAlmacen);
+llenarProductos("bebidas", bebidas, preciosBebidas, stockBebidas);
+llenarProductos("frescos", frescos, preciosFrescos, stockFrescos);
+llenarProductos("limpieza", limpieza, preciosLimpieza, stockLimpieza);
+llenarProductos("hogar", hogar, preciosHogar, stockHogar);
+
+let finalizarCompra = document.getElementById("finalizarCompra")
+finalizarCompra.addEventListener("click", () => alert(`Productos comprados: Total $${total}`))
